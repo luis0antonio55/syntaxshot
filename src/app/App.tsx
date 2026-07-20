4886dd687d5b9939db3005a6cd9265c773f64b58
luis0antonio55 <luis0antonio5@gmail.com>
2026-07-20 17:54:57 -0600

    feat: npm fixes and additon of vercel Speed Insight

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 
 export default function App() {
   return (
     <>
       <RouterProvider router={router} />
       <Analytics />
+       <SpeedInsights />
     </>
   );
 }
